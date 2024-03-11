902ca2a8241587e42e6daf51f87e591210cf1051
Alex Badea <alex.badea@keysight.com>
2024-03-11 14:48:59 -0700

    uet wireshark plugin: genesis
    Signed-off-by: Alex Badea <alex.badea@keysight.com>

---
+++ dissector/uet.lua
@@ -0,0 +1,19 @@
+-- UltraEthernet wireshark dissector
+-- Copyright Keysight Technologies 2024
+--
+-- To use this dissector, make it available in the wireshark plugins
+-- directory, for example:
+--
+-- ln -sf $(pwd)/uet.lua ~/.config/wireshark/plugins/
+
+p_uet = Proto("uet", "UltraEthernet Transport")
+p_uet.prefs["ip_proto"] = Pref.uint("IP protocol#", 253, "IP protocol number for UET")
+
+function p_uet.dissector(buf, pinfo, root)
+	pinfo.cols.protocol = p_uet.name
+end
+
+function p_uet:init()
+	local ip_table = DissectorTable.get("ip.proto")
+	ip_table:add(p_uet.prefs["ip_proto"], p_uet)
+end
