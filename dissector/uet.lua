da708bdb00dc5f89dfd4b0294d3818aea725809a
Alex Badea <alex.badea@keysight.com>
2024-03-11 16:43:59 -0700

    dissector: add flag bits
    Signed-off-by: Alex Badea <alex.badea@keysight.com>

--- dissector/uet.lua
+++ dissector/uet.lua
@@ -27,16 +27,33 @@ fld.type = ProtoField.uint16("uet.type", "Type", base.HEX, types, 0xf000)
 fld.flags = ProtoField.uint16("uet.flags", "Flags", base.HEX, nil, 0x0fe0)
 fld.next_hdr = ProtoField.uint16("uet.next_hdr", "Next Header", base.HEX, nil, 0x001f)
 
+-- N.B. masks shifted left by 5 bits (for next_hdr)
+fld.flag_syn = ProtoField.bool("uet.flags.syn", "SYN", 16, {"Yes", "No"}, 0x800)
+fld.flag_clr = ProtoField.uint16("uet.flags.clr", "CLR", base.DEC, nil, 0x0600)
+fld.flag_cc = ProtoField.bool("uet.flags.cc", "CC", 16, {"Yes - Congestion Control State present", "No"}, 0x0100)
+fld.flag_ar = ProtoField.bool("uet.flags.ar", "AR (ACK Requested)", 16, {"Yes", "No"}, 0x0080)
+fld.flag_retx = ProtoField.bool("uet.flags.retx", "RETX (is retransmit)", 16, {"Yes", "No"}, 0x0040)
+fld.flag_rsv = ProtoField.bool("uet.flags.rsv", "RSV", 16, {"Yes", "No"}, 0x0020)
+
 local dissect_data = Dissector.get("data")
 
 function p_uet.dissector(buf, pinfo, root)
 	pinfo.cols.protocol = p_uet.name
 
-	local subtree = root:add(p_uet, data_buf)
+	local subtree = root:add(p_uet, buf)
 	subtree:add(fld.entropy, buf(0, 2))
 	subtree:add(fld.type, buf(2, 2))
-	subtree:add(fld.flags, buf(2, 2))
+	local flags_tree = subtree:add(fld.flags, buf(2, 2))
 	subtree:add(fld.next_hdr, buf(2, 2))
+
+	-- TODO: per-type
+	flags_tree:add(fld.flags, buf(2, 2)) -- FIXME? helps eyeballing the bits for now.
+	flags_tree:add(fld.flag_syn, buf(2, 2))
+	flags_tree:add(fld.flag_clr, buf(2, 2))
+	flags_tree:add(fld.flag_cc, buf(2, 2))
+	flags_tree:add(fld.flag_ar, buf(2, 2))
+	flags_tree:add(fld.flag_retx, buf(2, 2))
+	flags_tree:add(fld.flag_rsv, buf(2, 2))
 
 	local h_type = buf(2, 1):bitfield(0, 4)
 	local type_str = types[h_type]
@@ -44,8 +61,8 @@ function p_uet.dissector(buf, pinfo, root)
 		pinfo.cols.info = type_str
 	end
 
-	local offset = 0
-	local pktlen = buf:len()
+	local offset = 4
+	local pktlen = buf:len() - offset
 	local data_buf = buf(offset, pktlen)
 	dissect_data:call(data_buf:tvb(), pinfo, subtree)
 end
